316f2f422cf9b31fa75e43f53e7ff0e292385787
ashkaty <ashkaty@gmail.com>
2024-12-05 00:18:28 -0600

    Logging Key Presses Globally via Rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,23 @@
+use rdev::{Event, listen, EventType};
+use std::thread;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
+fn callback(event: Event) {
+  
+    match event.event_type {
+        EventType::KeyPress(key) => println!("Key pressed {:?}", key),
+        EventType::KeyRelease(key) => println!("Key released {:?}", key),
+        _ => (),
+    }
+}
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    thread::spawn(|| listen(callback));
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
